2c3584b56753fcb7470d5cd6fc5a88c9be4c5da9
Aida N <nacarinobazaga@gmail.com>
2021-07-05 16:33:57 +0200

    FRONT-1010: add authReducer, uiReducer into combineReducers

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
+import { authReducer } from './reducers/authReducer';
+import { uiReducer } from './reducers/uiReducer';
 
 // import * as api from '../api';
 
 
 const reducers = combineReducers({
-
+    auth: authReducer,
+    ui: uiReducer
 });
 
 

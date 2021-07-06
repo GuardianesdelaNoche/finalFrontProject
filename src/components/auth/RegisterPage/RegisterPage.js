417c766834d53c67b21f98b5ac392ee15093ac45
Your Name <emgg2222@gmail.com>
2021-07-06 21:49:29 +0200

    EG-FRONT-1002: WIP add register page

---
+++ src/components/auth/RegisterPage/RegisterPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+const RegisterPage = props => {
+    return (
+        <h1> 
+            
+            <FormattedMessage
+                id = "register"
+                defaultMessage="Register"
+            />
+      </h1>
+    );
+}
+
+export default RegisterPage;
+
+

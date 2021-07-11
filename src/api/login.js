ddabae47f67dbcf4d9d0ae36e61e69dc882b6b56
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-07-11 11:29:11 +0200

    IG-FRONT-1003_login-Usuario:Change Headers

--- src/api/login.js
+++ src/api/login.js
@@ -2,19 +2,19 @@ import client, { configureClient } from './client';
 import storage from '../utils/storage';
 
 const version = 'v1';
-const baseURL = `https://services.4events.net/api/${version}/user`
+const baseURL = `/api/${version}/user`
 
-export const login = ({ remember, ...credentials}) => {
 
-	const url = `${baseURL}/login`;
-	return client.post(url, credentials).then(({ accessToken }) => {
-		configureClient({ accessToken });
-		return accessToken;
-	})
+export const login = ({ remember, ...credentials }) => {
+	return client
+		.post(`${baseURL}/login`, credentials)
+		.then(({ accessToken }) => {
+			configureClient({ accessToken });
+			return accessToken;
+		})
 		.then(accessToken => {
 			if (remember) {
 				storage.set('auth', accessToken);
 			}
-		}); 
-}
-
+		});
+};

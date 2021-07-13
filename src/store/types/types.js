b9d89344049b15f064737366423b77e846212198
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-07-13 13:19:26 +0200

    IG-FRONT.1016-Añadir-Button-Logout: Finish Logout Button

--- src/store/types/types.js
+++ src/store/types/types.js
@@ -2,6 +2,7 @@ export const types = {
     authLoginRequest: 'AUTH Login Request',
     authLoginSuccess: 'AUTH Login Success',
     authLoginError: 'AUTH Login Error',
+    authLogout: 'AUTH Logout',
 
     uiResetError: 'UI Reset Error'
 

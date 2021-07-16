ac7059b7b6216928547027751f6ac4fd480dd57b
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-07-16 11:57:26 +0200

    IG-FRONT-1006-Event-Details:Design and layout completed

--- src/components/events/EventDetail/EventDetail.js
+++ src/components/events/EventDetail/EventDetail.js
@@ -25,9 +25,8 @@ function EventDetail() {
 
 				
 							<div class="card-toolbar">
-								<button type="button" class="btn btn-sm btn-light">
-									Action
-								</button>
+								<a href="#" className="btn btn-light-danger">Eliminar</a>
+								
 							</div>
 						</div>
 						<div class="card-body">
@@ -39,7 +38,7 @@ function EventDetail() {
 
 
 							{/* Event Description */}
-							<p class="text-gray-800 fs-6 fw-normal pt-2 mb-2">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+							<p class="text-description fs-6 fw-normal pt-2 mb-2">"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
 
 							<h5 className="card-title align-items-start pt-4 flex-column">
 								<span className="fw-bolder text-dark">Details</span>
@@ -110,26 +109,40 @@ function EventDetail() {
 	
 
 								</div>
-
-						</div>
-						
-						<div class="card-footer">
-							<div class="separator mt-2"></div>
-							<div class="d-flex align-items-center pt-4">
-
-								<div className="symbol tags me-2">
-									<span className="bg-light-primary">
-										Aventure
-									</span>
+							
+					
+							<div className="separator mt-2 pt-2"></div>
+							<div className="d-flex ">
+								<div className="col d-flex flex-grow-1 my-lg-0 my-2 pe-3 pt-4">
+									
+										<div className="symbol tags me-2">
+											
+											<span className="bg-light-primary">
+												Aventure
+											</span>
+										</div>
+										<div className="symbol tags me-2">
+											<span className="bg-light-primary">
+												Sport
+											</span>
+										</div>
 								</div>
-								<div className="symbol tags me-2">
-									<span className="bg-light-primary">
-										Sport
+								<div className="col"></div>
+								<div className="col d-flex icons-footer my-lg-0 my-2 pe-3 pt-4">
+									<span className="btn btn-icon btn-sm btn-active-color-primary">
+										
+										<i className="fas fa-map-marker-alt fs-6"></i>
 									</span>
+
+									<span className="btn btn-icon btn-sm btn-active-color-primary">
+										<i className="fas fa-heart"></i>
+									</span>
+					
 								</div>
-				
 							</div>
+
 						</div>
+
 					</div>
 				</div>
 			</Layout>

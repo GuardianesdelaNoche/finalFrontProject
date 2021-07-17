aa57e658d2fba249fee95fc6ee5bbc2df61f0df9
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-07-17 19:50:17 +0200

    IG-FRONT--1015-Validacion-Login: fix Input form & WIP button validation

--- src/components/auth/LoginPage/LoginForm.js
+++ src/components/auth/LoginPage/LoginForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import useForm from '../../../hooks/useForm';
-import { FormattedMessage } from 'react-intl';
-import {Form} from '../../../components/shared/elements/formElements'
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Form, ContentBottomCenter, Button} from '../../../components/shared/elements/formElements'
 
-import { Input, Checkbok} from '../../shared/index'
-
+import { Checkbok } from '../../shared/index'
+import Input from '../../shared/components/Input';
 
 import './login.css'
 
@@ -26,46 +26,33 @@ function LoginForm({onSubmit}) {
 		remember: false,
 	});
 	const { email, password, remember } = credentials;
-
+	const intl = useIntl();
 	
 	return (
-		<Form className="form-signin" onSubmit={handleSubmit(onSubmit)}>
-
-				{/* <div className="form-container">
-					<label className="form-label">
-						<FormattedMessage
-							id="login.formLabel.email"
-							defaultMessage="Email"
-						/>
-					</label>
-
-					<Input 
-						type={'email'} 
-						name={'email'} 
-						required={'required'} 
-						placeholder={'name@username.com'} 
-						value={email} 
-						onChange={handleChange}
-					/>
-				</div>
-		
-				<div className="form-container">
-					<label className="form-label">
-						<FormattedMessage
-							id="login.formLabel.pass"
-							defaultMessage="Password"
-						/>
-					</label>
-				<Input 
-					type={'password'} 
-					name={'password'} 
-					required={'required'} 
-					placeholder={'********'} 
-					value={password} 
+		<form className="form-signin" onSubmit={handleSubmit(onSubmit)}>
+			<div className="form-container">
+				<Input
+					type="email"
+					label={intl.formatMessage({ id: 'login.formLabel.email' })}
+					name="email"
+					id="email"
+					placeholder="user@email.com"
+					value={email}
 					onChange={handleChange}
+					required
 				/>
-				</div> */}
-			
+
+				<Input
+					type="password"
+					label={intl.formatMessage({ id: 'login.formLabel.pass' })}
+					name="password"
+					id="password"
+					placeholder="******"
+					value={password}
+					onChange={handleChange}
+					required
+				/>
+
 				<Checkbok
 					type="checkbox"
 					name="remember"
@@ -74,20 +61,27 @@ function LoginForm({onSubmit}) {
 					checked={remember}
 				/>
 
-		{/* 	<Button variant="primary" disabled={!validate(validEmail, validPassword)}>
-				<FormattedMessage
-					id="login.form.button"
-					defaultMessage="Login"
-				/>
-			</Button> */}
+			</div>
 
 
-			<button
+			{/* <button
 				className="loginForm-submit"
 				disabled={!validate(validEmail, validPassword)}>Login</button>
+			*/}
 
+			<ContentBottomCenter>
 
-		</Form>
+				<Button
+					type="submit">
+					<FormattedMessage
+						id="login.form.button"
+						defaultMessage="Login"
+					/>
+				</Button>
+
+			</ContentBottomCenter> 
+
+		</form>
 	)
 }
 

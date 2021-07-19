a0d07dcdb8c876e8b2d9ffcab48594bb33027465
“GuardianesdelaNoche” <equipoguardianesdelanoche@gmail.com>
2021-07-19 16:27:13 +0200

    IG-FRONT-3008-Adaptar-Header-Responsive: WIP Responsive Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,10 @@
 import { NavLink, Link } from 'react-router-dom';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
 import Button from '../shared/Button';
 import AuthButton from '../auth/AuthButton/AuthButton';
 import './Header.css';
+
 
 
 
@@ -12,36 +13,93 @@ const isExact = match => match?.isExact;
 function Header() {
 	return (
 		<header>
-			<Navbar bg="light" variant="light" fixed="top">
+
+			<Navbar collapseOnSelect expand="lg" bg="light" variant="light" fixed="top" >
 				<Container>
-					<Navbar.Brand href="/">4EVENTS</Navbar.Brand>
-					<Nav className="me-auto">
-						<NavLink
-							to="/events/new"
-							className="menu-item"
-							isActive={isExact}
-						>
-							<FormattedMessage
+					<Navbar.Brand href="#home"><img alt="4EVENTS" src="..."></img></Navbar.Brand>
+					<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+					<Navbar.Collapse id="responsive-navbar-nav" className="menu">
+						<Nav className="me-auto">
+							<Nav.Link href="/events/new">
+								<FormattedMessage
 								id="navbar.item.menu"
 								defaultMessage="New Event"
-							/>
-							
-						</NavLink>
-						
-						<Button variant="secundary" className="me-2">
-							<Link to="/register">
-								<FormattedMessage
-									id="button.register.menu"
-									defaultMessage="Register"
 								/>
-							</Link> 
-						</Button>
+							</Nav.Link>
+						</Nav>
 						
+						<Nav >
+							<Navbar.Text className="signed me-2">
+								
+								<div class="btn btn-icon btn btn-icon btn-sm icon me-2">
+									<i class="fas fa-user"></i>
+								</div>
+								<a href="#login">Mark Otto</a>
+							{/* 	<div class="symbol symbol-55px me-2 mt-1">
+									<span class="symbol-label align-items-end">
+										<img alt="icon" src="/start-html-free/assets/media/svg/avatars/001-boy.svg" class="mh-40px">
+																		</span>
+																	</div> */}
+								
+							</Navbar.Text>
+								
 						
-						<AuthButton />
-					</Nav>
+							<Button variant="secundary" className="navbar-btn me-2">
+								<Link to="/register">
+									<FormattedMessage
+										id="button.register.menu"
+										defaultMessage="Register"
+									/>
+								</Link>
+							</Button>
+							<AuthButton className="navbar-btn" />
+						</Nav>
+					</Navbar.Collapse>
 				</Container>
 			</Navbar>
+			
+			{/* <Navbar className="navbar navbar-default" bg="light" variant="light" fixed="top">
+				<Container>
+					<div className="navbar-header">
+						<Navbar.Brand className="navbar-brand" href="/"> 
+						
+						</Navbar.Brand>
+					</div>
+					<Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+					<Navbar.Collapse>
+					
+						<Nav className="nav navbar-nav">
+							<NavLink
+								to="/events/new"
+								className="menu-item"
+								isActive={isExact}
+							>
+								<FormattedMessage
+									id="navbar.item.menu"
+									defaultMessage="New Event"
+								/>
+								
+							</NavLink>
+						</Nav>
+						<Nav className="nav navbar-nav navbar-right">
+						
+							<Button variant="secundary" className="navbar-btn me-2">
+								<Link to="/register">
+									<FormattedMessage
+										id="button.register.menu"
+										defaultMessage="Register"
+									/>
+								</Link> 
+							</Button>
+							
+							<AuthButton className="navbar-btn"/>
+						</Nav>
+
+
+					</Navbar.Collapse>
+
+				</Container>
+			</Navbar> */}
 
 		</header>
 	);

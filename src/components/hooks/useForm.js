54ad881c63a6a672bf5b8fdc26fddf005b516e1d
Your Name <emgg2222@gmail.com>
2021-07-23 16:59:48 +0200

    EG-FRONT-:WIP

--- src/components/hooks/useForm.js
+++ src/components/hooks/useForm.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React  from 'react';
 
 
 function useForm(initialState){
-	const [formValue, setFormValue] = React.useState({...initialState});
+	const [formValue, setFormValue] = React.useState((prevState) => ({...initialState}));
 
 	const handleChange = ({ target })=>{
 		setFormValue({

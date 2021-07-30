26a95155ba98ee9306fcb38c9aebf08250b87e7d
Aida N <nacarinobazaga@gmail.com>
2021-07-30 18:35:19 +0200

    AN-FRONT-1011-Paginacion-url: clean code & files

--- src/store/actions/pagination.js
+++ src/store/actions/pagination.js
@@ -1,28 +1,3 @@
-// import { types } from "../types/types";
-
-// export const paginationSetLimit = (numItems) => ({
-//     type: types.paginationSetLimit,
-//     payload: numItems
-// });
-
-// export const paginationSetTotalPages = (totalPages) => ({
-//     type: types.paginationSetTotalPages,
-//     payload: totalPages
-// });
-
-// export const paginationSetCurrentPage = (current) => ({
-//     type: types.paginationSetCurrentPage,
-//     payload: current
-// });
-
-
-// export const paginationUpdateCurrentPage = (beforePage, current) => {
-//     return function (dispatch, getState, { history }) {
-//         history.push(`/events/page/${beforePage}`);
-//         dispatch( paginationSetCurrentPage( current ) )
-//     }
-// }
-
 export const paginationRedirect = (path) => {
     return function(dispatch, getState, {history}) {
         history.push(path);
--- src/store/selectors/pagination.js
+++
@@ -1,3 +0,0 @@
-// export const getLimit = (state) => state.pagination.limit;
-// export const getCurrentPage = (state) => state.pagination.currentPage;
-// export const getTotalPages = (state) => state.pagination.totalPages;

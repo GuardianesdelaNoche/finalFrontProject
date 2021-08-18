44dd9567dc7b59cbe34677dc772acdfbecbbcd0f
Aida N <nacarinobazaga@gmail.com>
2021-08-18 09:00:18 +0200

    AN-FRONT-3016-Ajustes-listado-eventos: WIP - modify fav icon display and overlay

--- src/components/events/EventsPage/EventCard.js
+++ src/components/events/EventsPage/EventCard.js
@@ -7,8 +7,8 @@ import { BsCalendar, BsClock } from "react-icons/bs";
 import { RiHome4Line } from "react-icons/ri";
 import { TiTree } from "react-icons/ti";
 import { FormattedMessage } from "react-intl";
-import { GoLocation} from "react-icons/go";
-import { MdFavoriteBorder, MdFavorite} from "react-icons/md";
+import { GoLocation } from "react-icons/go";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 import { Button } from "react-bootstrap";
 import { useSelector } from "react-redux";
@@ -21,7 +21,6 @@ function EventCard(event) {
   // const state = useSelector(getIsEventFav(event._id));
   const isLogged = useSelector(getIsLogged);
 
-
   return (
     <Card className="card-stretch">
       <div>
@@ -30,116 +29,117 @@ function EventCard(event) {
             <OverlayTrigger
               overlay={
                 <Tooltip id="button-fav">
-                  {isLogged ? (
-                    <FormattedMessage
-                      id="eventCard.overlay.fav"
-                      defaultMessage="Register or Login"
-                    />
-                  ) : (
+                  {!isLogged && (
                     <FormattedMessage
                       id="eventCard.overlay.registerLogin"
+                      defaultMessage="Register or Login"
+                    />
+                  )}
+                  {isLogged && event.isFavorite && (
+                    <FormattedMessage
+                      id="eventCard.overlay.fav"
                       defaultMessage="Favourite"
                     />
+                  )}
+                  {isLogged && !event.isFavorite && 
+                  (
+                    <FormattedMessage
+                    id="eventCard.overlay.nofav"
+                    defaultMessage="No Favourite"
+                  />
                   )}
                 </Tooltip>
               }
             >
               {isLogged && event.isFavorite ? (
-                <Button
-                  className="ribbon home"
-                  variant="secondary"
-                  disabled
-                >
+                <div className="ribbon home p-2 bg-light rounded opacity-50">
                   <MdFavorite />
-                </Button>
+                </div>
               ) : (
-                <Button
-                  className="ribbon"
-                  variant="secondary"
-                  disabled
-                >
+                <div className="ribbon home p-2 bg-light rounded opacity-50">
                   <MdFavoriteBorder />
-                </Button>
+                </div>
               )}
             </OverlayTrigger>
-            <Link key={event._id} to={`/event/${event._id}/${event.title.replace(/\s+/g, '-')}`}>
-
-              <Card.Img variant="top"  className="home" src={event.photo} />
+            <Link
+              key={event._id}
+              to={`/event/${event._id}/${event.title.replace(/\s+/g, "-")}`}
+            >
+              <Card.Img variant="top" className="home" src={event.photo} />
             </Link>
           </span>
         </div>
       </div>
-      <Link key={event._id} to={`/event/${event._id}/${event.title.replace(/\s+/g, '-')}`}>
+      <Link
+        key={event._id}
+        to={`/event/${event._id}/${event.title.replace(/\s+/g, "-")}`}
+      >
         <Card.Body className="card">
           <Card.Title className="text-dark">{event.title}</Card.Title>
 
           <div className="d-flex flex-row justify-content-between  time  text-gray-800 ">
             <Card.Subtitle className="fs-6 fw-bolder">
-              <BsCalendar className="me-2"/> {moment(new Date(event.date)).format("DD-MM-YYYY")}
+              <BsCalendar className="me-2" />{" "}
+              {moment(new Date(event.date)).format("DD-MM-YYYY")}
             </Card.Subtitle>
             <Card.Subtitle className="fs-6 fw-bolder">
-              <BsClock className="me-2"/> {event.duration} h
+              <BsClock className="me-2" /> {event.duration} h
             </Card.Subtitle>
           </div>
           <div className="d-flex flex-row justify-content-between  time  text-gray-800 ">
             <Card.Subtitle className="fs-6 fw-bolder">
-              <GoLocation className="me-2"/> {event.city}
+              <GoLocation className="me-2" /> {event.city}
             </Card.Subtitle>
           </div>
           <Card.Text className="text-description">
             {event.description}
           </Card.Text>
 
-            <div className="row participant pt-4">
-              <div className="col d-flex">
-                <span>
-                  <i className="fas fa-users mw-75 me-2"></i>
+          <div className="row participant pt-4">
+            <div className="col d-flex">
+              <span>
+                <i className="fas fa-users mw-75 me-2"></i>
 
-                  {event.available_places}
+                {event.available_places}
+                <FormattedMessage
+                  id="eventCard.availableSeats"
+                  defaultMessage="Seats"
+                />
+              </span>
+            </div>
+            <div className="col d-flex float-right">
+              {event.indoor ? (
+                <div>
+                  <span className="me-2">
+                    <RiHome4Line />
+                  </span>
                   <FormattedMessage
-                    id="eventCard.availableSeats"
-                    defaultMessage="Seats"
+                    id="eventCard.indoor"
+                    defaultMessage="Indoor"
                   />
-                </span>
-              </div>
-              <div className="col d-flex float-right">
-                {event.indoor ? (
-                  <div>
-                    <span className="me-2">
-                      <RiHome4Line />
-                    </span>
-                    <FormattedMessage
-                      id="eventCard.indoor"
-                      defaultMessage="Indoor"
-                    />
-                  </div>
-                ) : (
-                  <div>
-                    <span className="me-2">
-                      <TiTree />
-                    </span>
-                    <FormattedMessage
-                      id="eventCard.outdoor"
-                      defaultMessage="Outdoor"
-                    />
-                  </div>
-                )}
-              </div>
+                </div>
+              ) : (
+                <div>
+                  <span className="me-2">
+                    <TiTree />
+                  </span>
+                  <FormattedMessage
+                    id="eventCard.outdoor"
+                    defaultMessage="Outdoor"
+                  />
+                </div>
+              )}
             </div>
-            <div>
-
-            </div>
-
-
+          </div>
+          <div></div>
 
           <div className="separator mt-2 pt-2"></div>
 
           <div className="row">
-
-
             <div className="col d-flex pt-4">
               <div className="symbol tags me-2">
-                <span className="bg-light-primary">{event.tags.map((tag) => `#${tag}`)}
+                <span className="bg-light-primary">
+                  {event.tags.map((tag) => `#${tag}`)}
                 </span>
               </div>
             </div>
@@ -150,7 +150,6 @@ function EventCard(event) {
             </div>
           </div>
         </Card.Body>
-
       </Link>
     </Card>
   );

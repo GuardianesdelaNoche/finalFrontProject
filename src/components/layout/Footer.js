7eb7155724c0d762f919430bc603b51a56c4c7c4
Aida N <nacarinobazaga@gmail.com>
2021-08-29 18:35:52 +0200

    AN-FRONT-3035-Fix-visualizar-iconos: fix display icon fav in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,8 +1,11 @@
+import { MdFavorite } from 'react-icons/md';
 import  './Footer.css';
 function Footer() {
 	return <footer>
 		<div className="container footer">
-			<span className="text-muted">Made with <i className="fas fa-heart"></i> by GuardianesdelaNoche</span>
+			<span className="text-muted">Made with 
+			 {" "}<MdFavorite />{" "} 
+			by GuardianesdelaNoche</span>
 		</div>
 	</footer>;
 }
